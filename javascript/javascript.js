af3cf9e291aee2d451152eda02656f6968d8ea62
Fatdog-Dev <81480856+Fatdog-Dev@users.noreply.github.com>
2021-08-02 10:54:24 +0100

    Add files via upload
    file for repo

---
+++ javascript/javascript.js
@@ -0,0 +1,15 @@
+document.getElementById('button', 'button-2').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'flex';
+});
+
+document.querySelector('.cancel-button').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'none';
+});
+
+document.getElementById('button-2').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'flex';
+});
+
+document.querySelector('.cancel-button').addEventListener('click', function() {
+    document.querySelector('.modal').style.display = 'none';
+});
